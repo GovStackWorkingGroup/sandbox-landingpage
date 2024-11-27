82ab9edfed87b58fce52446b00e4e4b96037564a
henry bobka <henry.bobka@gofore.com>
2024-11-27 12:06:13 +0100

    add vuetify + icons

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,38 @@
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// icons
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { fa } from 'vuetify/iconsets/fa'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+// styles
 import './assets/main.css'
 
+// components
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
+})
+
+// init app
 const app = createApp(App)
-
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
