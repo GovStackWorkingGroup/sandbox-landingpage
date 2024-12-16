3607e228fd61a02b977edb0dee4ca5adc7a2feff
henry bobka <henry.bobka@gofore.com>
2024-12-16 15:43:11 +0100

    exchange egov image

--- src/components/TheEgovPage.vue
+++ src/components/TheEgovPage.vue
@@ -17,7 +17,7 @@ const content = 'This is a demonstration to showcase GovStack Building Blocks in
 <style scoped>
 .egov-wrapper {
   position: relative;
-  height: 2600px;
+  height: 2640px;
   width: 100vw;
 }
 
@@ -29,11 +29,12 @@ const content = 'This is a demonstration to showcase GovStack Building Blocks in
   height: 2200px;
   margin-top: 2rem;
   border-radius: 20px;
-  background: url('@/assets/images/gs-demo-1200.png') center center no-repeat;
+  background: url('@/assets/images/gs-demo-2400.png') center center no-repeat;
+  background-size: 1200px;
   box-shadow: 0px 0px 20px rgba(0, 48, 135, 0.25);
-  backdrop-filter: blur(34.8px);
-  border: 0.5rem solid var(--gs-gray);
-  border-radius: 1rem;
+  /* backdrop-filter: blur(34.8px); */
+  /* border: 0.5rem solid var(--gs-gray); */
+  /* border-radius: 1rem; */
   box-sizing: content-box;
 }
 
