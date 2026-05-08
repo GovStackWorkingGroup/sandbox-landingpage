d87cb77e1f1b00667d45e09d720628b422ba4f42
nicolk <69297601+nicolk@users.noreply.github.com>
2026-05-08 18:04:16 +0200

    Update CrossBorderPaymentView.vue adjusted img ratio

--- src/views/CrossBorderPaymentView.vue
+++ src/views/CrossBorderPaymentView.vue
@@ -232,8 +232,8 @@ const { navigate } = useUI()
 /* demo image */
 
 .demo-image {
-  width: 592px;
-  height: auto;
+  width: 568px;
+  height: 389px;
   background: url('@/assets/images/cross-border-pay-demo-image.png') left center no-repeat;
 }
 
